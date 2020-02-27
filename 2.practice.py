527ea9b55cf88421322f1da5bb8b48df7789cdf4
toppeak <xkqvor123@gmail.com>
2020-02-28 03:00:59 +0900

    Create 2.practice.py

---
+++ 2.practice.py
@@ -0,0 +1,17 @@
+print(5) 
+print("풍선")
+print(5>10)
+print(5<10)
+print(not 5>10)
+print(not (5<10))
+
+#애완동물을 소개 해 주세요
+
+animal = "강아지"
+name = "연탄이"
+age = 4
+hobby ="산책"
+in_adult = age >=3
+print("우리집 " + animal +"의 이름은 "+name+ "이에여")
+print(name+"는(은)"+str(age)+" 살이며,"+hobby+"을 아주 좋아해요")
+print(name+"는(은)"+str(in_adult))
