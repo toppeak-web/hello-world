e15d4bac38e84059a54acda92132e4bdfbbba7ca
toppeak <xkqvor123@gmail.com>
2020-02-29 13:20:11 +0900

    py

--- 2.variable 변수 practice.py
+++ 2.variable 변수 practice.py
@@ -12,6 +12,9 @@ name = "연탄이"
 age = 4
 hobby ="산책"
 in_adult = age >=3
+'''안녕'''
+"""안녕"""
 print("우리집 " + animal +"의 이름은 "+name+ "이에여")
-print(name+"는(은)"+str(age)+" 살이며,"+hobby+"을 아주 좋아해요")
-print(name+"는(은)"+str(in_adult))
+# print(name+"는(은)"+str(age)+" 살이며,"+hobby+"을 아주 좋아해요")
+print(name, "는(은)", str(age)+" 살이며,", hobby, "을 아주 좋아해요")
+print(name+"는(은) 어른일까요? "+str(in_adult))
---
+++ 3.operator(연산자).py
@@ -0,0 +1,39 @@
+print(1+1)#2
+print(3-2)#1
+print(5*2)#10
+print(6/3) #2
+
+print(2**3) #2에3제곱 8
+print(5%3) #나머지 구하기 2
+print(15//4) #3
+
+print(10 > 3) #T
+print(4 >= 7) #F
+print(10 < 3) #F
+print(5 <= 6) #t
+
+print (3 == 3) #T
+print (3 == 4) #F
+print(3 + 4 == 7) #T
+
+print(1 != 3)#T
+print(not(1 != 3)) #F
+
+print((3 > 0) and (3 < 5)) #T
+print((3 > 0) & (3 < 5)) #T
+
+print((3 > 0) or (6 < 5)) #T
+
+print((3 > 0) | (6 < 5)) #T
+
+# 간단한 수식
+print(2+3*4) #14
+print((2+3)*4)#20
+num = 2*8//4 #4
+print(num)
+num +=2 #num = num + 2
+print(num)
+num *=2 #6*2=12
+print(num)
+num /= 4 #12나누기4=3
+print(num)
