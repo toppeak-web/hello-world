95ba5cb204018caf678998524cd764a637ac2620
toppeak <xkqvor123@gmail.com>
2020-02-29 17:56:35 +0900

    123

---
+++ 4.numerical processing function(숫자 처리 함수).py
@@ -0,0 +1,11 @@
+print(abs(-5)) #5 절대값
+print(pow(4, 2)) #4의2제곱
+print(max(5, 12)) #12
+print(min(5, 12)) #5
+print(round(3.141592)) #3
+print(round(1.7899)) #2
+
+from math import * #math 라이브러리의 모든것을 이용
+print(floor(4.99)) #내림 4
+print(ceil(3.14)) #올림 4
+print(sqrt(16)) #제곱근 4
