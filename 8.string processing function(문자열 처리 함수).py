e2b45e8f44591363cbfea4123943665d4bdf1786
toppeak <xkqvor123@gmail.com>
2020-02-29 22:40:45 +0900

    Update 8.string processing function(문자열 처리 함수).py

--- 8.string processing function(문자열 처리 함수).py
+++ 8.string processing function(문자열 처리 함수).py
@@ -1,4 +1,4 @@
-py = "Python is Amzing"
+py = "Python is Amazing"
 print(py.upper()) #대문자
 print(py.lower()) #소문자
 print(py[0].isupper())
@@ -7,4 +7,7 @@ print(len(py))
 print(py.replace("Python","Java"))
 
 index = py.index("n")
+print(index)
+
+index = py.index("n", index + 1)
 print(index)
