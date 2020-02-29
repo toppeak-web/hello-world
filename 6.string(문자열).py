0852e72b8610f58cff74f356cf31b29b5547146b
toppeak <xkqvor123@gmail.com>
2020-02-29 22:29:05 +0900

    d

---
+++ 6.string(문자열).py
@@ -0,0 +1,8 @@
+sentence = "나는 소년입니다."
+print(sentence)
+sentence2 = "반갑습니다."
+print(sentence2)
+sentence3 = """
+안녕 하세요
+반갑습니다."""
+print(sentence3)
---
+++ 8.string processing function(문자열 처리 함수).py
@@ -0,0 +1,10 @@
+py = "Python is Amzing"
+print(py.upper()) #대문자
+print(py.lower()) #소문자
+print(py[0].isupper())
+print(py[1].isupper())
+print(len(py))
+print(py.replace("Python","Java"))
+
+index = py.index("n")
+print(index)
