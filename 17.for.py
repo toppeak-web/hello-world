8ba3b4ef52ee046e62dd48ff460b32415e5fd10e
toppeak <xkqvor123@gmail.com>
2020-03-03 18:02:15 +0900

    123

---
+++ 17.for.py
@@ -0,0 +1,8 @@
+# print("대기번호 : 1")
+
+# for waiting_no in range(1, 6): #[1, 2, 3, 4, 5]:
+#     print("대기번호 : {0}".format(waiting_no))
+
+startbuks = ["아이언맨", "토르", "아이엠 그루트"]
+for customer in startbuks:
+    print("{0}, 커피가 준비되었습니다.".format(customer))
